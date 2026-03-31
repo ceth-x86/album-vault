a3186bbe046932cd0ff39a1e71c085cf63d29b6c
Andrei Demidov <mail@demas.cc>
2026-03-31 17:23:41 +0200

    just add site

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  outDir: '../dist',
+  site: 'https://ceth-x86.github.io',
+  base: '/album-vault',
+});
